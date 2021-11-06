fd819968f2a45aac3acf8807f55bb8aff5f4cf04
Alexander Samarkin <alexgui@yandex.ru>
2021-11-06 16:33:58 +0300

    fixed export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { Robokassa } from './robokassa';
 import type { RobokassaConfig, Order, ReceiptItem } from './types';
 import { SNO, PaymentObject, PaymentMethod, PaymentTax } from './types';
 
-export { Robokassa };
+export { Robokassa, SNO, PaymentTax, PaymentMethod, PaymentObject };
 
-export type { RobokassaConfig, ReceiptItem, Order, SNO, PaymentTax, PaymentMethod, PaymentObject };
+export type { RobokassaConfig, ReceiptItem, Order };
